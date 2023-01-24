2491fad973eb1af5030ed751678ef7b96a9ce073
areeba00 <areebaakhawar@gmail.com>
2023-01-24 15:02:26 +0500

    home page filter added

--- src/components/home-page/job-categories.jsx
+++ src/components/home-page/job-categories.jsx
@@ -7,216 +7,44 @@ import { Link } from "react-router-dom";
 import './card.css';
 import displayImage from "../../assets/finance.png";
 
-function Jobs() {
+import { getNewArrivals } from "../../redux/actions/homejob";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import Card1 from "../student-page/Card";
+
+const Jobs = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getNewArrivals());
+      }, [dispatch]);
+    
+      const { newArrivals } = useSelector((state) => state.filter);
+
     return (
-        <React.Fragment>
             <div class="bg-slate-50">
                 <div class="self-center text-2xl font-semibold whitespace-nowrap text-center text-gray-900 pt-8"> Browse Categories </div>
                 <div class="self-center font-normal whitespace-nowrap text-center text-gray-900 pb-8" > Most popular categories of portal sorted by poularity
                 </div>
+                {/* {JSON.stringify(newArrivals)} */}
                 <div class="container mx-auto">
+                    
                     <div class="flex flex-wrap -mx-4">
-                        <div class="w-full sm:w-1/4 md:w-1/4 xl:w-1/5 p-4">
-                            <Card sx={{
-                                maxWidth: 175,
-                                maxHeight: 300,
-                                margin: "0 auto",
-                                padding: "0.1em",
-                            }}>
-
-                                <div class="flex flex-col items-center">
-
-                                    <CardMedia
-                                        component="img"
-                                        image={displayImage}
-                                        alt="finance"
-                                        sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
+                    {newArrivals && newArrivals.map(newArrival => (
+                                    <Card1
+                                      key={newArrivals._id}
+                                      job={newArrival}
                                     />
-
-                                    <CardContent>
-                                        <h5 class="text-2xl text-gray-900 dark:text-white">Finance</h5>
-                                        <p class="font-normal text-gray-700 dark:text-gray-400">
-                                            (4228) jobs
-                                        </p>
-                                    </CardContent>
-
-                                </div>
-                                <CardActions>
-                                    <div className="Button">
-                                        <Link to="/" class="font-medium text-indigo-600 hover:text-indigo-700"> View All</Link>
-                                    </div>
-                                </CardActions>
-                            </Card>
-                        </div>
-                        <div class="w-full sm:w-1/4 md:w-1/4 xl:w-1/5 p-4">
-                            <Card sx={{
-                                maxWidth: 175,
-                                maxHeight: 300,
-                                margin: "0 auto",
-                                padding: "0.1em",
-                            }}>
-
-                                <div class="flex flex-col items-center">
-
-                                    <CardMedia
-                                        component="img"
-                                        image={displayImage}
-                                        alt="finance"
-                                        sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
-                                    />
-
-                                    <CardContent>
-                                        <h5 class="text-2xl text-gray-900 dark:text-white">Finance</h5>
-                                        <p class="font-normal text-gray-700 dark:text-gray-400">
-                                            (4228) jobs
-                                        </p>
-                                    </CardContent>
-
-                                </div>
-                                <CardActions>
-                                    <div className="Button">
-                                        <Link to="/" class="font-medium text-indigo-600 hover:text-indigo-700"> View All</Link>
-                                    </div>
-                                </CardActions>
-                            </Card>
-                        </div>
-                        <div class="w-full sm:w-1/4 md:w-1/4 xl:w-1/5 p-4">
-                            <Card sx={{
-                                maxWidth: 175,
-                                maxHeight: 300,
-                                margin: "0 auto",
-                                padding: "0.1em",
-                            }}>
-
-                                <div class="flex flex-col items-center">
-
-                                    <CardMedia
-                                        component="img"
-                                        image={displayImage}
-                                        alt="finance"
-                                        sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
-                                    />
-
-                                    <CardContent>
-                                        <h5 class="text-2xl text-gray-900 dark:text-white">Finance</h5>
-                                        <p class="font-normal text-gray-700 dark:text-gray-400">
-                                            (4228) jobs
-                                        </p>
-                                    </CardContent>
-
-                                </div>
-                                <CardActions>
-                                    <div className="Button">
-                                        <Link to="/" class="font-medium text-indigo-600 hover:text-indigo-700"> View All</Link>
-                                    </div>
-                                </CardActions>
-                            </Card>
-                        </div>
-                        <div class="w-full sm:w-1/4 md:w-1/4 xl:w-1/5 p-4">
-                            <Card sx={{
-                                maxWidth: 175,
-                                maxHeight: 300,
-                                margin: "0 auto",
-                                padding: "0.1em",
-                            }}>
-
-                                <div class="flex flex-col items-center">
-
-                                    <CardMedia
-                                        component="img"
-                                        image={displayImage}
-                                        alt="finance"
-                                        sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
-                                    />
-
-                                    <CardContent>
-                                        <h5 class="text-2xl text-gray-900 dark:text-white">Finance</h5>
-                                        <p class="font-normal text-gray-700 dark:text-gray-400">
-                                            (4228) jobs
-                                        </p>
-                                    </CardContent>
-
-                                </div>
-                                <CardActions>
-                                    <div className="Button">
-                                        <Link to="/" class="font-medium text-indigo-600 hover:text-indigo-700"> View All</Link>
-                                    </div>
-                                </CardActions>
-                            </Card>
-                        </div>
-                        <div class="w-full sm:w-1/4 md:w-1/4 xl:w-1/5 p-4">
-                            <Card sx={{
-                                maxWidth: 175,
-                                maxHeight: 300,
-                                margin: "0 auto",
-                                padding: "0.1em",
-                            }}>
-
-                                <div class="flex flex-col items-center">
-
-                                    <CardMedia
-                                        component="img"
-                                        image={displayImage}
-                                        alt="finance"
-                                        sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
-                                    />
-
-                                    <CardContent>
-                                        <h5 class="text-2xl text-gray-900 dark:text-white">Finance</h5>
-                                        <p class="font-normal text-gray-700 dark:text-gray-400">
-                                            (4228) jobs
-                                        </p>
-                                    </CardContent>
-
-                                </div>
-                                <CardActions>
-                                    <div className="Button">
-                                        <Link to="/" class="font-medium text-indigo-600 hover:text-indigo-700"> View All</Link>
-                                    </div>
-                                </CardActions>
-                            </Card>
-                        </div>
-                        <div class="w-full sm:w-1/4 md:w-1/4 xl:w-1/5 p-4">
-                            <Card sx={{
-                                maxWidth: 175,
-                                maxHeight: 300,
-                                margin: "0 auto",
-                                padding: "0.1em",
-                            }}>
-
-                                <div class="flex flex-col items-center">
-
-                                    <CardMedia
-                                        component="img"
-                                        image={displayImage}
-                                        alt="finance"
-                                        sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
-                                    />
-
-                                    <CardContent>
-                                        <h5 class="text-2xl text-gray-900 dark:text-white">Finance</h5>
-                                        <p class="font-normal text-gray-700 dark:text-gray-400">
-                                            (4228) jobs
-                                        </p>
-                                    </CardContent>
-
-                                </div>
-                                <CardActions>
-                                    <div className="Button">
-                                        <Link to="/" class="font-medium text-indigo-600 hover:text-indigo-700"> View All</Link>
-                                    </div>
-                                </CardActions>
-                            </Card>
-                        </div>
+                                  ))
+                                  }
                     </div>
 
                     
                 </div>
+
                 <div class="pb-8"></div>
 
             </div>
 
-        </React.Fragment>
     );
 }
 
--- src/components/student-page/Card.js
+++ src/components/student-page/Card.js
@@ -18,56 +18,60 @@ const Card1 = ({ job }) => {   // job used in this line is the variable job used
 	// };
 
 	return (
-	// 	<div>
-			
-	// 			<figure class="grid-rows-4 items-center mb-8 bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-	// 			<img class="w-20 h-20  " src={displayImage} alt="company name" />
-	// 				<div class="flex flex-col justify-between p-4 leading-normal">
-	// 					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{job.jobTitle}</h5>
-	// 					<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{job.jobDescription}</p>
-	// 				</div>
-	// 			</figure>
 
-	// </div>
+			// <div class="max-w-sm mx-4 my-4 px-4 py-4 rounded overflow-hidden shadow-lg">
+			// 		<img class=" object-contain" src={displayImage}  alt="Sunset in the mountains"/>
+			// 		<div class="px-6 py-4">
+			// 		<div class="font-bold text-xl mb-2 text-center">{job.jobTitle}</div>
+			// 		<p class="text-gray-700 text-base text-center">
+			// 		{job.jobDescription.length > 20 ? job.jobDescription.substring(0,20) + '.....' : job.jobDescription.substring(0,20)}
+			// 		</p>
+			// </div>
+			//  		<Link
+ 			// 					to={`/job/${job._id}`}
+ 			// 					type='button'
+ 			// 					class='text-blue-500 pl-16 underline hover:text-blue-700 text-center'
+ 			// 				>
+ 			// 					View Details
+ 			// 				</Link>
+			// </div>
 
-// 	<div >
-// 		<div class="max-w-sm mb-8 px-8 mr-8 my-4 py-4 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-//     <a href="#">
-//         <img class="w-24 h-24 object-center" src={displayImage} alt="" />
-//     </a>
-//     <div class="p-5">
-//         <a href="#">
-//             <h5 class="mb-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">{job.jobTitle}</h5>
-//         </a>
-//         <p class="mb-3 font-normal text-center text-gray-700 dark:text-gray-400">{job.jobDescription}</p>
+			<div class="container mx-auto">
+			<div class="flex flex-wrap -mx-4">
+			<div class="w-full sm:w-1/4 md:w-1/4 xl:w-1/5 p-4">
+                            <Card sx={{
+                                maxWidth: 175,
+                                maxHeight: 300,
+                                margin: "0 auto",
+                                padding: "0.1em",
+                            }}>
 
-// 		<Link
-// 								to={`/job/${job._id}`}
-// 								type='button'
-// 								class='text-blue-500 ml-12 underline hover:text-blue-700'
-// 							>
-// 								View Details
-// 							</Link>
-//     </div>
-// </div>
-// 	</div>
+                                <div class="flex flex-col items-center">
 
-			<div class="max-w-sm mx-4 my-4 px-4 py-4 rounded overflow-hidden shadow-lg">
-					<img class="w-full" src={displayImage} alt="Sunset in the mountains"/>
-					<div class="px-6 py-4">
-					<div class="font-bold text-xl mb-2 text-center">{job.jobTitle}</div>
-					<p class="text-gray-700 text-base text-center">
-					{job.jobDescription.length > 20 ? job.jobDescription.substring(0,20) + '.....' : job.jobDescription.substring(0,20)}
-					</p>
-			</div>
-			 		<Link
- 								to={`/job/${job._id}`}
- 								type='button'
- 								class='text-blue-500 pl-16 underline hover:text-blue-700 text-center'
- 							>
- 								View Details
- 							</Link>
-			</div>
+                                    <CardMedia
+                                        component="img"
+                                        image={displayImage}
+                                        alt="finance"
+                                        sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
+                                    />
+
+                                    <CardContent>
+                                        <h5 class="text-2xl text-gray-900 dark:text-white">{job.jobTitle}</h5>
+                                        <p class="font-normal text-gray-700 dark:text-gray-400">
+										 		{job.jobDescription.length > 20 ? job.jobDescription.substring(0,20) + '.....' : job.jobDescription.substring(0,20)}
+                                        </p>
+                                    </CardContent>
+
+                                </div>
+                                <CardActions>
+                                    <div className="Button">
+                                        <Link to={`/job/${job._id}`} class="font-medium text-indigo-600 hover:text-indigo-700"> View All</Link>
+                                    </div>
+                                </CardActions>
+                            </Card>
+                        </div>
+				</div>
+				</div>
 
 	);
 };
